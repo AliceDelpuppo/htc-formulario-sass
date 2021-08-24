b5482f52a5335f948a87a08be4d7e8884a9b54d2
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-08-23 22:47:51 -0300

    Continuação validação dos inputs

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 const $name = document.querySelector('input[data-type="name"]')
 const $email = document.querySelector('input[data-type="email"]')
 
+const $birthDate = document.querySelector('.input[data-type="birth-date"]')
+
 const REGEX_DICTIONARY = {
     name: /^[a-zzáàâãéèêíóôõúç][a-zzáàâãéèêíóôõúç ]*$/i,
-    email: /^[a-z][^@ ]*[@][a-z]+([.][a-z]+)+$/i
+    email: /^[a-z][^@ ]*[@][a-z]+([.][a-z]+)+$/i,
+    birthDate: /[^0-9]/g
 }
-
-$name.addEventListener('input', validateInput)
-
-$email.addEventListener('input', validateInput)
 
 function validateInput() {
     const value = this.value
@@ -25,3 +24,39 @@ function validateInput() {
         this.classList.add('error')
     }
 }
+
+$name.addEventListener('input', validateInput)
+$email.addEventListener('input', validateInput)
+
+$birthDate.addEventListener('input', function () {
+    const regex = REGEX_DICTIONARY.birthDate
+    const value = this.value.replace(regex, '')
+
+    this.value = value.slice(0, 8)
+
+
+    if (this.value.length == 8 || this.value.length == 0) {
+        this.classList.remove('error')
+    } else {
+        this.classList.add('error')
+    }
+})
+
+$birthDate.addEventListener('blur', function () {
+    const value = this.value
+
+    if (this.value.length == 8) {
+        const day = value[0] + value[1]
+        const month = value[2] + value[3]
+        const year = value.slice(4)
+
+        this.value = `${day}/${month}/${year}`
+    }
+})
+
+$birthDate.addEventListener('focus', function () {
+    const regex = REGEX_DICTIONARY.birthDate
+    const value = this.value.replace(regex, '')
+
+    this.value = value
+})
